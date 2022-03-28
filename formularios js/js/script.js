1426ae65a2335c595001d6c5b8d5963646169151
JesusGalea <jrgaleasantiago98@gmail.com>
2022-03-28 13:35:22 +0200

    update

---
+++ formularios js/js/script.js
@@ -0,0 +1,7 @@
+	function ObtenerDatos(){
+	var name = document.getElementById('nombre').value;
+	var name2 = document.getElementById('apellidos').value;
+	var age = document.getElementById('edad').value;
+
+	alert(name + name2 + age);
+}
